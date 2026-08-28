fd0b395b7cc3ad75fce0e45ca403470e6cd2f3bb
nawabzadahacker0-beep <nawabzadahacker0@gmail.com>
2026-08-27 18:48:48 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB25VaErJEsI3VLBeb52cpczKRmEWC4fEs",
+    authDomain: "pak-earning-site.firebaseapp.com",
+    databaseURL: "https://pak-earning-site-default-rtdb.firebaseio.com",
+    projectId: "pak-earning-site",
+    storageBucket: "pak-earning-site.firebasestorage.app",
+    messagingSenderId: "830671389706",
+    appId: "1:830671389706:web:16ab555ffdd85cff70cbf3"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.database();
