8f10daecc93ffff2179f3ed4845fa08e31ab50ea
nawabzadahacker0-beep <nawabzadahacker0@gmail.com>
2026-08-28 08:37:50 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,8 +8,10 @@ const firebaseConfig = {
     appId: "1:830671389706:web:16ab555ffdd85cff70cbf3"
 };
 
+// Initialize Firebase (only once)
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
+// 🔴 Using Realtime Database
 const db = firebase.database();
